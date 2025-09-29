4d5e21c23989ae7e76eae02a0060a5913115f368
Nicholas Martino <147353791+MartinoSwaggins@users.noreply.github.com>
2025-09-28 22:29:23 -0400

    Add files via upload

---
+++ sp2.c
@@ -0,0 +1,56 @@
+/**********************************************************
+ *
+ * sampleProgram1.c
+ * CIS 451 Lab 5
+ *
+ * !!!Nick Martino!!!
+ *************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 4096
+
+int main()
+{
+    int shmId;
+    char *sharedMemoryPtr;
+    struct shmid_ds shmInfo;
+
+    if ((shmId = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+        perror("Unable to get shared memory\n");
+        exit(1);
+    }
+
+    if ((sharedMemoryPtr = shmat(shmId, 0, 0)) == (void*) -1) {
+        perror("Unable to attach\n");
+        exit(1);
+    }
+
+    if (shmctl(shmId, IPC_STAT, &shmInfo) < 0) {
+        perror("Unable to get shared memory info\n");
+        exit(1);
+    }
+
+    printf("Shared Memory ID: %d\n", shmId);
+    printf("Value a: %p\t Value b: %p\n", (void *) sharedMemoryPtr, (void *) sharedMemoryPtr + SHM_SIZE);
+    printf("Size of shared memory: %zu bytes\n", shmInfo.shm_segsz);
+
+    pause();
+
+    if (shmdt(sharedMemoryPtr) < 0) {
+        perror("Unable to detach\n");
+        exit(1);
+    }
+
+    if (shmctl(shmId, IPC_RMID, 0) < 0) {
+        perror("Unable to deallocate\n");
+        exit(1);
+    }
+
+    return 0;
+}
